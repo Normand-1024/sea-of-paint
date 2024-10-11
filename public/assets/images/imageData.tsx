ddfd73f18ee9ece5e45eab608986088ee6b41a31
kkosolap <kkosolap@ucsc.edu>
2024-10-11 13:47:08 -0700

    adding images and worked on automating image blending

--- public/assets/images/imageData.tsx
+++ public/assets/images/imageData.tsx
@@ -1,15 +1,29 @@
 export const IMAGE_DATA : Array<any> = [
     {   
-        "name": "1",
-        "descp": "image descriptions",
-        "keywords": ["keyword1", "keyword2", "keyword3"],
+        "name": "city",
+        "descp": "city buildings corporate job work data unfulfilled sitting desk typing computer boss",
+        "keywords": ["city", "job", "unfulfilled"],
         "questions": "questions",
         "unlocked": "unlockedText"
     },
     {   
-        "name": "2",
-        "descp": "image descriptions",
-        "keywords": ["keyword1", "keyword2", "keyword3"],
+        "name": "lily",
+        "descp": "girl red walking cigarette backpack school highschool rebellious",
+        "keywords": ["girl", "walking", "freedom"],
+        "questions": "questions",
+        "unlocked": "unlockedText"
+    },
+    {   
+        "name": "mey_sitting_2",
+        "descp": "woman sitting chair desk pensive thinking bored reflecting house",
+        "keywords": ["woman", "sitting", "pensive"],
+        "questions": "questions",
+        "unlocked": "unlockedText"
+    },
+    {   
+        "name": "mey_crowd",
+        "descp": "woman crowd busy blur uncertain afraid",
+        "keywords": ["woman", "crowd", "uncertain"],
         "questions": "questions",
         "unlocked": "unlockedText"
     },
