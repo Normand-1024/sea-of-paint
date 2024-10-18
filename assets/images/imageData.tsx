5eb275afd6b3a91917c1006ceb2faf73205cb831
Normand-1024 <Normand-1024@users.noreply.github.com>
2024-10-18 08:03:18 +0000

    deploy: 13836876884dc91e64800867a11b8c5e3d6ec7c8

---
+++ assets/images/imageData.tsx
@@ -0,0 +1,39 @@
+export const IMAGE_DATA : Array<any> = [
+    {   
+        "name": "city",
+        "descp": "city buildings",
+        "keywords": [],
+        "info": "city",
+        "interpretations": [],
+        "path": []
+    },
+    {   
+        "name": "lily",
+        "descp": "Lily walking smoking",
+        "keywords": ["guilty", "hope", "uncertainty"],
+        "info": "lily",
+        "interpretations": [["This image shows Lily's self-reliance",
+                                    "Lily's pose of walking, and the sense of speed in the movement. They show Lily's determination to face the future."
+                            ],
+                            ["This image shows Lily's loneliness and pain",
+                                    "Her expression feels detached. The external coldness of her figure hides a person bearing something difficult deep within."
+                            ]],
+        "path": ["p_lily_initial"]
+    },
+    {   
+        "name": "mey_sitting_2",
+        "descp": "Mey sitting thinking home",
+        "keywords": [],
+        "info": "mey_sitting_2",
+        "interpretations": [],
+        "path": []
+    },
+    {   
+        "name": "mey_crowd",
+        "descp": "Mey crowd uncertain",
+        "keywords": [],
+        "info": "mey_crowd",
+        "interpretations": [],
+        "path": []
+    },
+];
